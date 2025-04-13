12a8b99a6c1e11cd94b6cc610c7633d9a5f33895
Grif_on <91139654+grif-on@users.noreply.github.com>
2025-04-13 20:49:14 +0300

    Organised script options

--- versigm.ps1
+++ versigm.ps1
@@ -1,9 +1,34 @@
-$options_path = "../options/windows/options_windows.yy"
+function scriptOptions() {
+	
+	# Replace tryToGetProjectDirectory with regullar path if script placed not inside project folder
+	$global:project_directory = tryToGetProjectDirectory
+	
+	# Options file to consider having always actual version number
+	$global:master_options_path = "$global:project_directory/options/windows/options_windows.yy"
+	
+}
+
+
+#region Helper functions
+
+function tryToGetProjectDirectory() {
+	
+	return ".." # todo - actually implement , lol
+	
+}
+
+#endregion Helper functions
+
+
+scriptOptions
+
+
+#region Main script part
 
 # Note
 # ConvertFrom-Json/ConvertTo-Json are simplier to use but they have different json formating comparing to gamemaker
 
-$options = Get-Content -Path $options_path
+$options = Get-Content -Path $global:master_options_path
 
 # testing
 $version_line = $options[31].Split("`"")
@@ -12,4 +37,6 @@ $options[31] = $version_line -join "`""
 
 $options_string = $options -join "`n"
 
-Set-Content -NoNewline -Path $options_path -Value $options_string
+Set-Content -NoNewline -Path $global:master_options_path -Value $options_string
+
+#endregion Main script part
